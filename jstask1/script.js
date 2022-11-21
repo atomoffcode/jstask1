ca99174af32ecb140c2b39933b30feb15f6afa84
Kamil Abdullayev <112484288+atomoffcode@users.noreply.github.com>
2022-11-21 13:26:37 +0400

    v2

--- jstask1/script.js
+++ jstask1/script.js
@@ -3,15 +3,22 @@ var y = x.getHours()
 
 if(y>=6 && y<12){
     document.body.style.backgroundColor = 'yellow'
+    alert('Sabahin xeyr')
 }
 else if(y>=12 && y<18){
     document.body.style.backgroundColor = 'orange'
+    alert('Gunortan xeyr')
+
 }
 else if(y>=18 && y<0){
     document.body.style.backgroundColor = 'blue'
+    alert('Axsamin xeyr')
+
 }
 else if(y>=0 && y<6){
     document.body.style.backgroundColor = 'black'
+    alert('Gecen xeyre')
+
 }
 
 
